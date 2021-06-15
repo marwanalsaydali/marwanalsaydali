fa45d548ed064f3164324de4029c2d03f4118181
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-06-15 11:58:13 -0400

    added desc to the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "marwanalsaydali",
+    title: "MarwanAlsaydali",
+    bio:
+      "Father, Husband, freelance web developer and photographer that loves to tell stories through beautiful websites and images.",
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
 };
