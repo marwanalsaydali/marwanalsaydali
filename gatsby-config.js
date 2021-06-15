6fb7135285ebe7de3656e76e5ac2a2923d1e88d1
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-06-15 14:05:33 -0400

    contact info to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: "MarwanAlsaydali",
-    bio:
-      "Father, Husband, freelance web developer and photographer that loves to tell stories through beautiful websites and images.",
+    name: "Marwan Alsaydali",
+    jobtitle: "Web Developer",
+    bio: "Father, Husband, freelance web developer and photographer that loves to tell stories through beautiful websites and images.",
+    links: {
+      twitter: "https://twitter.com/alsaydali",
+      instagram: "https://instagram.com/alsaydali",
+      github: "https://github.com/marwanalsaydali",
+      linkedin: "https://www.linkedin.com/in/marwanalsaydali/",
+      email: "marwanalsaydali@gmail.com",
+    },
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-gatsby-cloud"],
 };
